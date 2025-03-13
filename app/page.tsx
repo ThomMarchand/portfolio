b5612bb2a4bb74a1eec89c3d5dcfec88a60b92b4
Thomas <thomasmarchand63@gmail.com>
2025-03-13 19:47:02 +0100

    feat(main): added section

--- app/page.tsx
+++ app/page.tsx
@@ -5,74 +5,56 @@ import profilPic from "@/public/images/profil.png";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center md:items-end">
+    <main className="text-black md:static flex flex-col items-center md:items-end">
       <Image
         src={profilPic}
         alt="Picture of the author"
-        className="size-40 mt-50 md:mt-4 md:mr-10"
+        className="md:absolute size-40 mt-50 md:mt-4 md:mr-10"
       />
 
       <div className="md:flex">
         <Navbar />
 
         <div className="flex-1 overflow-auto px-5 md:ml-70 md:h-screen lg:ml-85 xl:ml-140">
-          <h2
-            id="about"
-            className="mb-5 text-4xl font-bold mt-4 scroll-mt-96 md:scroll-mt-30"
-          >
-            À propos
-          </h2>
+          <section>
+            <h2
+              id="about"
+              className="mb-5 text-4xl font-bold mt-4 scroll-mt-64 md:scroll-mt-30 md:mt-30"
+            >
+              À propos
+            </h2>
 
-          <p className="text-lg pt-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
-            nulla, obcaecati error rem aut tempora officia nostrum dolorum unde
-            ea! Magni ad fugit ipsam labore vitae rerum minima, inventore,
-            explicabo est iure, enim omnis. Voluptatum iusto fugit doloribus
-            delectus modi, officiis, repellat officia vel reprehenderit,
-            sapiente assumenda? Quo fuga voluptates aliquam pariatur, sequi,
-            molestiae nulla necessitatibus, autem repudiandae eaque delectus
-            quasi beatae quibusdam nobis magnam ducimus minus similique ex
-            facere culpa nisi alias sint laborum! Voluptatem, vel quaerat illo
-            velit iusto illum provident aut nesciunt, commodi vero sit aliquam
-            tempora, nisi inventore! Veniam ex deserunt ut officia cumque nihil
-            alias hic dignissimos iusto harum libero, earum sint expedita quos
-            vitae explicabo vero atque necessitatibus pariatur voluptas
-            consequuntur. Necessitatibus vitae sit voluptatum ex provident
-            eveniet inventore porro. Officia eveniet temporibus odio non alias
-            harum sequi nobis soluta, aliquam similique ullam veritatis porro
-            nihil provident quas sed fugiat voluptatum maiores laudantium
-            consequuntur iure rerum! Sint officia incidunt numquam quo aliquid
-            fugit? Perferendis deserunt voluptas, eligendi similique labore
-            ipsam sequi cupiditate temporibus id itaque, quod suscipit ea
-            placeat eaque doloribus, doloremque delectus non neque culpa
-            provident accusamus pariatur omnis enim! Nostrum, molestias
-            obcaecati optio repudiandae soluta illo enim reprehenderit delectus
-            cumque reiciendis iste incidunt, tempora exercitationem facilis
-            eligendi non numquam deserunt omnis tempore aliquid, atque culpa
-            accusantium. Aliquam nam sunt facere officiis excepturi nostrum
-            quisquam laborum facilis quas. Nam aliquam totam vel et voluptates
-            quas earum, voluptatem exercitationem quia laborum. Illo, harum
-            quisquam commodi quasi totam nisi officiis nam provident maiores
-            quam quas, quod dolorem soluta, nobis distinctio possimus
-            perferendis! Accusamus eos dicta aliquam libero dignissimos illum
-            nostrum illo id vitae quaerat pariatur temporibus explicabo,
-            inventore sint natus vel ab quis fuga. Maxime quis tempore quibusdam
-            perspiciatis eius saepe tempora sapiente quae ex praesentium unde
-            ipsum nobis totam repellendus non rerum quo, veniam autem laudantium
-            omnis fuga eaque architecto soluta. Deleniti dignissimos velit vel,
-            ad explicabo consectetur facilis natus consequatur omnis facere
-            quos, perferendis inventore, veniam voluptates distinctio cumque
-            mollitia dolore minima. Iste magnam id delectus? Possimus, quasi
-            adipisci. Repudiandae ad at maxime! Quibusdam doloribus deserunt et
-            quis ex cum odio dicta nemo vel, officia natus porro aliquid quidem
-            voluptatem ut dignissimos, eos quaerat totam iusto consequuntur
-            repellendus reiciendis! Magni, vero. Cum quisquam cumque et, eos
-            debitis esse eum soluta quod veniam rem. In asperiores laborum quia
-            cum eveniet facere consectetur, ducimus eum vel inventore
-            voluptatibus, sit illum molestiae repudiandae, odio optio. Vitae?
-          </p>
+            <p className="text-lg pt-5">
+              Créer des applications de A à Z, développer des fonctionnalités
+              performantes et enquêter sur les bugs pour en identifier l’origine
+              et les corriger avec précision est ce qui me passionne.
+            </p>
 
-          <div className="my-20">
+            <p className="text-lg pt-2">
+              Depuis toujours, l’informatique me passionne, que ce soit sur le
+              plan personnel, à travers les jeux vidéo et ma curiosité pour
+              Internet, ou dans ma vie professionnelle. Lors de mon passage chez
+              les Compagnons du Devoir, j’ai découvert la conception assistée
+              par ordinateur (CAO). Plus tard, chez Euromaster, l’omniprésence
+              de l’informatique a renforcé mon attrait pour ce domaine.
+            </p>
+
+            <p className="text-lg pt-2">
+              C’est un ami administrateur système qui m’a permis de mieux
+              comprendre le métier de développeur et de réaliser qu’il était
+              accessible.
+            </p>
+
+            <p className="text-lg pt-2">
+              J’ai alors commencé par apprendre les bases du développement Web
+              en construisant des applications Front-end. Après plusieurs mois
+              d’apprentissage en autodidacte, j’ai choisi d’approfondir mes
+              compétences en intégrant la formation de Concepteur Développeur
+              d’Applications en alternance à la Wild Code School.
+            </p>
+          </section>
+
+          <section className="my-20">
             <h2
               id="xp"
               className="mb-5 text-4xl font-bold scroll-mt-64 md:scroll-mt-30"
@@ -130,63 +112,67 @@ export default function Home() {
               eum vel inventore voluptatibus, sit illum molestiae repudiandae,
               odio optio. Vitae?
             </p>
-          </div>
+          </section>
 
-          <h2
-            id="project"
-            className="mb-5 text-4xl font-bold scroll-mt-64 md:scroll-mt-30"
-          >
-            Projects
-          </h2>
+          <section>
+            <h2
+              id="project"
+              className="mb-5 text-4xl font-bold scroll-mt-64 md:scroll-mt-30"
+            >
+              Projects
+            </h2>
 
-          <p className="text-lg pt-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
-            nulla, obcaecati error rem aut tempora officia nostrum dolorum unde
-            ea! Magni ad fugit ipsam labore vitae rerum minima, inventore,
-            explicabo est iure, enim omnis. Voluptatum iusto fugit doloribus
-            delectus modi, officiis, repellat officia vel reprehenderit,
-            sapiente assumenda? Quo fuga voluptates aliquam pariatur, sequi,
-            molestiae nulla necessitatibus, autem repudiandae eaque delectus
-            quasi beatae quibusdam nobis magnam ducimus minus similique ex
-            facere culpa nisi alias sint laborum! Voluptatem, vel quaerat illo
-            velit iusto illum provident aut nesciunt, commodi vero sit aliquam
-            tempora, nisi inventore! Veniam ex deserunt ut officia cumque nihil
-            alias hic dignissimos iusto harum libero, earum sint expedita quos
-            vitae explicabo vero atque necessitatibus pariatur voluptas
-            consequuntur. Necessitatibus vitae sit voluptatum ex provident
-            eveniet inventore porro. Officia eveniet temporibus odio non alias
-            harum sequi nobis soluta, aliquam similique ullam veritatis porro
-            nihil provident quas sed fugiat voluptatum maiores laudantium
-            consequuntur iure rerum! Sint officia incidunt numquam quo aliquid
-            fugit? Perferendis deserunt voluptas, eligendi similique labore
-            ipsam sequi cupiditate temporibus id itaque, quod suscipit ea
-            placeat eaque doloribus, doloremque delectus non neque culpa
-            provident accusamus pariatur omnis enim! Nostrum, molestias
-            obcaecati optio repudiandae soluta illo enim reprehenderit delectus
-            cumque reiciendis iste incidunt, tempora exercitationem facilis
-            eligendi non numquam deserunt omnis tempore aliquid, atque culpa
-            accusantium. Aliquam nam sunt facere officiis excepturi nostrum
-            quisquam laborum facilis quas. Nam aliquam totam vel et voluptates
-            quas earum, voluptatem exercitationem quia laborum. Illo, harum
-            quisquam commodi quasi totam nisi officiis nam provident maiores
-            quam quas, quod dolorem soluta, nobis distinctio possimus
-            perferendis! Accusamus eos dicta aliquam libero dignissimos illum
-            nostrum illo id vitae quaerat pariatur temporibus explicabo,
-            inventore sint natus vel ab quis fuga. Maxime quis tempore quibusdam
-            perspiciatis eius saepe tempora sapiente quae ex praesentium unde
-            ipsum nobis totam repellendus non rerum quo, veniam autem laudantium
-            omnis fuga eaque architecto soluta. Deleniti dignissimos velit vel,
-            ad explicabo consectetur facilis natus consequatur omnis facere
-            quos, perferendis inventore, veniam voluptates distinctio cumque
-            mollitia dolore minima. Iste magnam id delectus? Possimus, quasi
-            adipisci. Repudiandae ad at maxime! Quibusdam doloribus deserunt et
-            quis ex cum odio dicta nemo vel, officia natus porro aliquid quidem
-            voluptatem ut dignissimos, eos quaerat totam iusto consequuntur
-            repellendus reiciendis! Magni, vero. Cum quisquam cumque et, eos
-            debitis esse eum soluta quod veniam rem. In asperiores laborum quia
-            cum eveniet facere consectetur, ducimus eum vel inventore
-            voluptatibus, sit illum molestiae repudiandae, odio optio. Vitae?
-          </p>
+            <p className="text-lg pt-5">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Molestiae nulla, obcaecati error rem aut tempora officia nostrum
+              dolorum unde ea! Magni ad fugit ipsam labore vitae rerum minima,
+              inventore, explicabo est iure, enim omnis. Voluptatum iusto fugit
+              doloribus delectus modi, officiis, repellat officia vel
+              reprehenderit, sapiente assumenda? Quo fuga voluptates aliquam
+              pariatur, sequi, molestiae nulla necessitatibus, autem repudiandae
+              eaque delectus quasi beatae quibusdam nobis magnam ducimus minus
+              similique ex facere culpa nisi alias sint laborum! Voluptatem, vel
+              quaerat illo velit iusto illum provident aut nesciunt, commodi
+              vero sit aliquam tempora, nisi inventore! Veniam ex deserunt ut
+              officia cumque nihil alias hic dignissimos iusto harum libero,
+              earum sint expedita quos vitae explicabo vero atque necessitatibus
+              pariatur voluptas consequuntur. Necessitatibus vitae sit
+              voluptatum ex provident eveniet inventore porro. Officia eveniet
+              temporibus odio non alias harum sequi nobis soluta, aliquam
+              similique ullam veritatis porro nihil provident quas sed fugiat
+              voluptatum maiores laudantium consequuntur iure rerum! Sint
+              officia incidunt numquam quo aliquid fugit? Perferendis deserunt
+              voluptas, eligendi similique labore ipsam sequi cupiditate
+              temporibus id itaque, quod suscipit ea placeat eaque doloribus,
+              doloremque delectus non neque culpa provident accusamus pariatur
+              omnis enim! Nostrum, molestias obcaecati optio repudiandae soluta
+              illo enim reprehenderit delectus cumque reiciendis iste incidunt,
+              tempora exercitationem facilis eligendi non numquam deserunt omnis
+              tempore aliquid, atque culpa accusantium. Aliquam nam sunt facere
+              officiis excepturi nostrum quisquam laborum facilis quas. Nam
+              aliquam totam vel et voluptates quas earum, voluptatem
+              exercitationem quia laborum. Illo, harum quisquam commodi quasi
+              totam nisi officiis nam provident maiores quam quas, quod dolorem
+              soluta, nobis distinctio possimus perferendis! Accusamus eos dicta
+              aliquam libero dignissimos illum nostrum illo id vitae quaerat
+              pariatur temporibus explicabo, inventore sint natus vel ab quis
+              fuga. Maxime quis tempore quibusdam perspiciatis eius saepe
+              tempora sapiente quae ex praesentium unde ipsum nobis totam
+              repellendus non rerum quo, veniam autem laudantium omnis fuga
+              eaque architecto soluta. Deleniti dignissimos velit vel, ad
+              explicabo consectetur facilis natus consequatur omnis facere quos,
+              perferendis inventore, veniam voluptates distinctio cumque
+              mollitia dolore minima. Iste magnam id delectus? Possimus, quasi
+              adipisci. Repudiandae ad at maxime! Quibusdam doloribus deserunt
+              et quis ex cum odio dicta nemo vel, officia natus porro aliquid
+              quidem voluptatem ut dignissimos, eos quaerat totam iusto
+              consequuntur repellendus reiciendis! Magni, vero. Cum quisquam
+              cumque et, eos debitis esse eum soluta quod veniam rem. In
+              asperiores laborum quia cum eveniet facere consectetur, ducimus
+              eum vel inventore voluptatibus, sit illum molestiae repudiandae,
+              odio optio. Vitae?
+            </p>
+          </section>
         </div>
       </div>
     </main>
